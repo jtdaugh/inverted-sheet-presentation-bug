210eb034f2705a1e66c3bd6abb26aec4e527cc07
Jesse Daugherty <jesse.daugherty@albert.com>
2025-05-23 14:54:25 -0700

    Initial Commit

---
+++ Repro/ReproApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReproApp.swift
+//  Repro
+//
+//  Created by Jesse Daugherty on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ReproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
